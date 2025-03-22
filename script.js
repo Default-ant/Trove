99175d012b406ec8d71f939b995caccb6408d245
Default-ant <krishnamani468@gmail.com>
2025-03-22 21:16:42 +0000

    Initial commit: Added base files for Travel Trove

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to simulate the start of an adventure
+function startAdventure() {
+    alert("🎮 Let the retro travel adventure begin!");
+}
+
+// Function to display travel places
+function showPlace(placeName) {
+    alert(`🌏 Exploring ${placeName}! Get ready to dive in.`);
+}
